0fff7db9bf566683023bd7b753b5f761d7329c8b
Gabriel Luque <GabJL@users.noreply.github.com>
2024-11-30 20:00:05 +0100

    Create variables.py

---
+++ clases/c20/variables.py
@@ -0,0 +1,17 @@
+# Ejercicio 1 y 2
+
+Málaga: dict = {"enero": 12, "febrero": 12, "marzo": 14, "abril": 16, "mayo": 19, "junio": 23, "julio": 26, "agosto": 26,"septiembre": 23, "octubre": 19, "noviembre": 15, "diciembre": 13}
+
+Yakustk: dict = {"enero": -38, "febrero": -34, "marzo": -20, "abril": -5, "mayo": 8, "junio": 16, "julio": 20, "agosto": 15, "septiembre": 6, "octubre": -8, "noviembre": -27, "diciembre": -37}
+
+# Ejercicio 3
+Salud: dict = {
+  "Bioquímica estructural": [0.66, 0.61, 0.47, 0.64, 0.92, 1.0, 0.84, 0.61, 0.79, 0.87], 
+  "Cálculo": [0.55, 0.52, 0.48, 0.29, 0.29, 0.11, 0.36, 0.77, 0.63, 0.71], 
+  "Fundamentos de la programación": [0.58, 0.66, 0.53, 0.76, 0.7, 0.74, 0.58, 0.81, 0.74, 0.89], 
+  "Física I": [0.23, 0.41, 0.38, 0.45, 0.37, 0.48, 0.62, 0.51, 0.67, 0.75], 
+  "Álgebra lineal": [0.48, 0.44, 0.46, 0.54, 0.61, 0.49, 0.57, 0.72, 0.73, 0.79]
+}
+
+# Ejercicio 4
+peli = "en Deadpool & Wolverine == Action, Comedy, Science Fiction == Shawn Levy == Lewis Tan, Ed Kear, Nick Pauley, Hugh Jackman == 128.0 200000000.0 1337900827.0 7.7 24-7-2024 == A listless Wade Wilson toils away in civilian life with his days as the morally flexible mercenary, Deadpool, behind him. But when his homeworld faces an existential threat, Wade must reluctantly suit-up again with an even more reluctant Wolverine. "
